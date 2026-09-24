fa44dd53ba4c89f8a39d7814f1da5fa5f6061d01
Green-rosee <82574142+Green-rosee@users.noreply.github.com>
2026-09-24 16:57:04 +0300

    Update README

---
+++ WebAppEstimate/Areas/SiteBram/Models/WinchAnchorModelValid.cs
@@ -0,0 +1,6 @@
+namespace WebAppEstimate.Areas.SiteBram.Models;
+
+public class WinchAnchorModelValid
+{
+    
+}
---
+++ WebAppEstimate/Areas/SiteBram/Services/WinchAnchorService.cs
@@ -0,0 +1,6 @@
+namespace WebAppEstimate.Areas.SiteBram.Services;
+
+public class WinchAnchorService
+{
+    
+}
